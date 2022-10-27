242b58930c57a1802b3be5638ec870e57f856e97
ktalimaa <kristeltalimaa@gmail.com>
2022-10-27 20:56:49 +0300

    Exception examples added

--- src/main/java/org/sda/Main.java
+++ src/main/java/org/sda/Main.java
@@ -6,6 +6,7 @@ import org.sda.services.implementations.PersonServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
+
         // INTERFACE
         Person person = new Person();
         person.setFirstName("Kristel");
@@ -19,5 +20,22 @@ public class Main {
         System.out.println("Persons birth year: " + personService.getPersonBirthYear(person.getAge()));
         System.out.println("Persons full name: " + personService.getPersonFullName(person));
         System.out.println("Persons last name: " + person.getLastName());
+
+        // EXCEPTION
+        // Exception handling
+        try {
+            int[] intArray = {1, 4, 56, 7};
+
+            for (int i = 0; i <= intArray.length; i++) {
+                System.out.println(intArray[i]);
+            }
+        } catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException) {
+            System.out.println("The number cannot be printed because its unavailable in the array.");
+        } catch (Exception exception) {
+            System.out.println("Exception being caught");
+        } finally { // This block will be executed irrespective of catch blocks
+            int a = 15;
+            System.out.println("Finally executed, a:" + a);
+        }
     }
 }
