ae497fa17920ee8002bc0574c74570ebd10ebadd
ktalimaa <kristeltalimaa@gmail.com>
2022-10-27 18:29:42 +0300

    Enum override examples added

--- src/main/java/org/sda/Main.java
+++ src/main/java/org/sda/Main.java
@@ -85,6 +85,12 @@ public class Main {
         System.out.println(PaymentType.CARD);        // prints enum CARD
         System.out.println(Arrays.toString(PaymentType.values()));   // will print all the enum values
         System.out.println(PaymentType.BANK_TRANSFER.getValue());     // prints the value which is set to enums in paymentType class, for example 3
+
+        for(PaymentType paymentType: PaymentType.values()) {
+            System.out.println(paymentType.name());     // for loop for enums, always use name() not toString()
+        }
+
+        System.out.println(PaymentType.MOBILE_BANKING.toString());
     }
 
     private static void printPersonAddress(Person person) {
--- src/main/java/org/sda/model/PaymentType.java
+++ src/main/java/org/sda/model/PaymentType.java
@@ -8,15 +8,18 @@ package org.sda.model;
 
 // all enums are in capital letter, if two words X_X, use down stripe
 public enum PaymentType {
-    CARD (1),
-    CASH (2),
-    BANK_TRANSFER (3),
-    MOBILE_BANKING (4),
-    BANK_LINK (5);
+    CARD (1, "Card"),
+    CASH (2, "Cash"),
+    BANK_TRANSFER (3, "Bank transfer"),
+    MOBILE_BANKING (4, "Mobile banking"),
+    BANK_LINK (5, "Bank link");
 
     private int value;
-    PaymentType(int value) {
+    private String name;
+
+    PaymentType(int value, String name) {
         this.value = value;
+        this.name = name;
     }
 
     public int getValue() {
@@ -25,5 +28,21 @@ public enum PaymentType {
 
     public void setValue(int value) {
         this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Passenger's PaymentType{" +
+                "value=" + value +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
