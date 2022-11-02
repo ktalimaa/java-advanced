30b996ab0bb455eb3d6508994c57a71607873e7d
ktalimaa <kristeltalimaa@gmail.com>
2022-11-02 20:23:21 +0200

    List examples added

--- src/main/java/org/sda/Main.java
+++ src/main/java/org/sda/Main.java
@@ -3,6 +3,10 @@ package org.sda;
 import org.sda.generics.*;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.SimpleTimeZone;
 
 public class Main {
     public static void main(String[] args) {
@@ -25,7 +29,7 @@ public class Main {
         System.out.println(genericFoodWithString.getItem());
 
         // GENERIC TYPE - EXTENDS
-        Vegetable  vegetable = new Vegetable();
+        Vegetable vegetable = new Vegetable();
         GenericExtendFood<Vegetable> vegetableGenericExtendFood = new GenericExtendFood<>(vegetable);
         System.out.println(vegetableGenericExtendFood.getRating());
 
@@ -34,5 +38,40 @@ public class Main {
         Snack snack2 = new Snack(3);
         System.out.println("Snack 1 is better than snack 2: " + snack1.isBetter(snack2));
 
+        // LIST
+        List<String> animalList = new ArrayList<>();
+        animalList.add("Lion");     // 0
+        animalList.add("Tiger");    // 1
+        animalList.add("Dog");      // 2
+        animalList.add("Cat");
+        animalList.add("Bear");
+        animalList.add("Panda");
+
+        System.out.println(animalList.get(1));      // prints tiger
+
+        animalList.remove(1);       // removing item from the list, if nr 1, tiger will be deleted
+        animalList.remove("Dog");
+
+        for (String animal : animalList) {
+            System.out.println(animal);
+        }
+
+        Iterator<String> stringIterator = animalList.listIterator();       // is similar to scanner.hasNext()
+
+        while (stringIterator.hasNext()) {
+            System.out.println(stringIterator.next() + " ");
+        }
+
+        // List of items to be removed
+        List<String> removeAnimalList = new ArrayList<>();
+        removeAnimalList.add("Cat");
+        removeAnimalList.add("Panda");
+
+        animalList.removeAll(removeAnimalList);
+
+        // Final removed list
+        for (String animal : animalList) {
+            System.out.println(animal);
+        }
     }
 }
