e902b5730ac1d52840a41d8d646e3d9620f344a0
ktalimaa <kristeltalimaa@gmail.com>
2022-11-07 21:15:13 +0200

    Lambda hw added

---
+++ src/main/java/org/sda/lambdahomework/Addition.java
@@ -0,0 +1,15 @@
+package org.sda.lambdahomework;
+
+/**
+ * Create and present the usage of lambda expressions:
+ * a) Addition, subtraction, multiplication, division.
+ * b) The sum of elements (int type) of the list.
+ * c) Number of words in the input expression (list containing elements of type String).
+ * d) * List before and after sorting (use the Array class and lambda expressions:
+ * String::compareToIgnoreCase as a comparator)
+ *
+ * @author Kristel Talimaa
+ */
+
+public class Addition {
+}
