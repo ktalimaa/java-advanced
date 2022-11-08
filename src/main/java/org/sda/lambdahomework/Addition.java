960122a1c8d22deb42d40ad54b784923ac8b1e6e
ktalimaa <kristeltalimaa@gmail.com>
2022-11-08 21:40:27 +0200

    Lambda hw started

--- src/main/java/org/sda/lambdahomework/Addition.java
+++ src/main/java/org/sda/lambdahomework/Addition.java
@@ -1,5 +1,9 @@
 package org.sda.lambdahomework;
 
+import java.util.ArrayList;
+import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
+
 /**
  * Create and present the usage of lambda expressions:
  * a) Addition, subtraction, multiplication, division.
@@ -12,4 +16,31 @@ package org.sda.lambdahomework;
  */
 
 public class Addition {
+    public static void main(String[] args) {
+
+        // Addition, subtraction, multiplication, division.
+
+        UnaryOperator<Integer> toAddition = x -> x + x;
+        System.out.println(toAddition.apply(5));
+
+        int d = 4;
+        UnaryOperator<Integer> toSubtraction = y -> y - d;
+        System.out.println(toSubtraction.apply(10));
+
+        int k = 3;
+        UnaryOperator<Integer> toMultiplication = l -> d * k;
+        System.out.println(toMultiplication.apply(0));
+
+        int l = 60;
+        UnaryOperator<Integer> toDivision = m -> l / k;
+        System.out.println(toDivision.apply(0));
+
+
+        // The sum of elements (int type) of the list.
+
+
+
+
+
+    }
 }
