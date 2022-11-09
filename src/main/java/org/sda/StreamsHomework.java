e1b794a656c96242d5cfb33b5a73ef9f531dd46e
ktalimaa <kristeltalimaa@gmail.com>
2022-11-09 11:19:08 +0200

    Streams hw started

---
+++ src/main/java/org/sda/StreamsHomework.java
@@ -0,0 +1,18 @@
+package org.sda;
+
+/**
+ * Using streams for a given lists:
+ * ["John", "Sarah", "Mark", "Tyla", "Ellisha", "Eamonn"]
+ * [1, 4, 2346, 123, 76, 11, 0, 0, 62, 23, 50]
+ * a) Sort the list.
+ * b) Print only those names, that start with "E" letter.
+ * c) Print values greater than 30 and lower than 200.
+ * d) Print names uppercase.
+ * e) Remove first and last letter, sort and print names.
+ * * Sort backwards by implementing reverse Comparator and using lambda expression.
+ *
+ * @author Kristel Talimaa
+ */
+
+public class StreamsHomework {
+}
